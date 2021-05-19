4745c784bbcd4aae22fa5c0192674724cea01122
Urmish Shah <33273194+surmish@users.noreply.github.com>
2021-05-19 16:49:51 -0700

    Create config.lua

---
+++ lua/config.lua
@@ -0,0 +1,53 @@
+require('plugins')
+
+vim.cmd 'autocmd BufWritePost plugins.lua PackerCompile'
+
+require("zen-mode").setup {
+  window = {
+    backdrop = 0.95, -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
+    -- height and width can be:
+    -- * an absolute number of cells when > 1
+    -- * a percentage of the width / height of the editor when <= 1
+    width = 0.8, -- width of the Zen window
+    height = 0.8, -- height of the Zen window
+    -- by default, no options are changed for the Zen window
+    -- uncomment any of the options below, or add other vim.wo options you want to apply
+    options = {
+      signcolumn = "no", -- disable signcolumn
+      number = false, -- disable number column
+      relativenumber = false, -- disable relative numbers
+      -- cursorline = false, -- disable cursorline
+      -- cursorcolumn = false, -- disable cursor column
+      -- foldcolumn = "0", -- disable fold column
+      -- list = false, -- disable whitespace characters
+    },
+  },
+  plugins = { 
+    tmux = { enabled = true },
+  }
+}
+
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ignore_install = { "javascript" }, -- List of parsers to ignore installing
+  highlight = {
+    enable = true,              -- false will disable the whole extension
+    disable = {},  -- list of language that will be disabled
+  },
+}
+
+require('neoscroll').setup()
+
+require('bufferline').setup {
+  options = {
+    numbers = "ordinal" ,
+    number_style = { "none", "subscript" }, -- buffer_id at index 1, ordinal at index 2
+    mappings = true ,
+    diagnostics = "nvim_lsp",
+    persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
+    -- can also be a table containing 2 custom separators
+    -- [focused and unfocused]. eg: { '|', '|' }
+    show_buffer_icons = false,
+    separator_style = { '', '' },
+  }
+}
