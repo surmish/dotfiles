e836043b966dc38a60fabd5c576d6d8fa78f393a
Urmish Shah <33273194+surmish@users.noreply.github.com>
2021-07-06 23:34:34 -0700

    Update init.lua

--- init.lua
+++ init.lua
@@ -5,25 +5,122 @@ source ~/.vimrc
 ]]
 
 require('packer').startup(function()
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
-    -- Post-install/update hook with neovim command
-    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-    use { 'folke/zen-mode.nvim' }
-    use { 'sakhnik/nvim-gdb' }
-    use { 'karb94/neoscroll.nvim' }
-    use { 'akinsho/nvim-bufferline.lua', requires = 'kyazdani42/nvim-web-devicons' }
-    use {'ms-jpq/chadtree', run = function()
-      vim.fn.system("python3 -m chadtree deps")
-      vim.cmd("CHADdeps")
-    end}
-    use { 'folke/tokyonight.nvim' }
-    use {"npxbr/gruvbox.nvim", requires = {"rktjmp/lush.nvim"}}
-    use { 'hoob3rt/lualine.nvim', requires = {'kyazdani42/nvim-web-devicons', opt = true} }
-    use { 'nvim-telescope/telescope.nvim', requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}}
+  use 'wbthomason/packer.nvim'
+  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+  use 'nvim-treesitter/playground'
+  use 'nvim-treesitter/nvim-treesitter-textobjects'
+  use 'nvim-treesitter/nvim-treesitter-refactor'
+  use 'p00f/nvim-ts-rainbow'
+  use 'folke/zen-mode.nvim'
+  use 'sakhnik/nvim-gdb'
+  use 'karb94/neoscroll.nvim'
+  use { 'akinsho/nvim-bufferline.lua', requires = 'kyazdani42/nvim-web-devicons' }
+  use {'ms-jpq/chadtree', run = function()
+    vim.fn.system("python3 -m chadtree deps")
+    vim.cmd("CHADdeps")
+  end}
+  use 'eddyekofo94/gruvbox-flat.nvim'
+  use { 'hoob3rt/lualine.nvim', requires = {'kyazdani42/nvim-web-devicons', opt = true} }
+  use { 'nvim-telescope/telescope.nvim', requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}}
+
+  use 'L3MON4D3/LuaSnip' -- snippet-engine
+  use 'rafamadriz/friendly-snippets'
+  use 'hrsh7th/nvim-compe'
+  -- Collection of common configurations for the Nvim LSP client
+  use 'neovim/nvim-lspconfig'
+  -- Extensions to built-in LSP, for example, providing type inlay hints
+  use 'nvim-lua/lsp_extensions.nvim'
+  use 'simrat39/rust-tools.nvim'
+  use({
+    "aserowy/tmux.nvim",
+    config = function()
+      require("tmux").setup({
+        -- overwrite default configuration
+        -- here, e.g. to enable default bindings
+        navigation = {
+          -- enables default keybindings (C-hjkl) for normal mode
+          enable_default_keybindings = true,
+        },
+        resize = {
+          -- enables default keybindings (A-hjkl) for normal mode
+          enable_default_keybindings = true,
+        }
+      })
+    end
+  })
 end)
 
-require('lualine').setup()
+local on_attach = function(client)
+  require'completion'.on_attach(client)
+end
+require'lspconfig'.pyright.setup{}
+require'lspconfig'.clangd.setup {
+  default_config = { 
+    capabilities = capabilities; 
+    cmd = { "clangd", "--background-index", "--pch-storage=memory", "--clang-tidy", "--suggest-missing-includes", "--cross-file-rename" }, 
+    filetypes = {"c", "cpp", "objc", "objcpp"}, 
+    init_options = { 
+      clangdFileStatus     = true,
+      usePlaceholders      = true,
+      completeUnimported   = true,
+      semanticHighlighting = false
+    }, 
+    root_dir = require'lspconfig'.util.root_pattern("compile_flags.txt","apbld","compile_commands.json") 
+  }, 
+  on_attach = on_attach_common 
+}
+require'lspconfig'.rust_analyzer.setup({
+  on_attach=on_attach,
+  settings = {
+    ["rust-analyzer"] = {
+      assist = {
+        importGranularity = "module",
+        importPrefix = "by_self",
+      },
+      cargo = {
+        loadOutDirsFromCheck = true
+      },
+      procMacro = {
+        enable = true
+      },
+    }
+  }
+})
+
+require('lualine').setup{
+  options = {
+    -- theme = 'gruvbox-flat'
+    -- theme = 'gruvbox'
+    -- theme = 'everforest'
+    theme = 'dracula'
+  }
+}
+
+vim.o.completeopt = "menuone,noselect"
+require('compe').setup {
+  enabled = true;
+  autocomplete = true;
+  debug = false;
+  min_length = 1;
+  preselect = 'enable';
+  throttle_time = 80;
+  source_timeout = 200;
+  incomplete_delay = 400;
+  max_abbr_width = 100;
+  max_kind_width = 100;
+  max_menu_width = 100;
+  documentation = true;
+  source = {
+    path = true;
+    buffer = true;
+    calc = true;
+    nvim_lsp = true;
+    nvim_lua = true;
+    vsnip = true;
+    ultisnips = true;
+    luasnip = true;
+  };
+}
 
 require("zen-mode").setup {
   window = {
@@ -51,53 +148,217 @@ require("zen-mode").setup {
 }
 
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-  ignore_install = { "javascript" }, -- List of parsers to ignore installing
-  highlight = {
-    enable = true,              -- false will disable the whole extension
-    disable = {},  -- list of language that will be disabled
-  },
+	ensure_installed = {"c","cpp","rust","python","lua","commonlisp","verilog","bash"}, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+	-- ignore_install = { "javascript" }, -- List of parsers to ignore installing
+	highlight = {
+		enable  = true, -- false will disable the whole extension
+		disable = {},   -- list of language that will be disabled
+	},
+	indent = {
+		enable = true
+	},
+	rainbow = {
+		enable = true
+	},
+	incremental_selection = {
+		enable = true,
+		keymaps = {
+			init_selection = "gnn",
+			node_incremental = "grn",
+			scope_incremental = "grc",
+			node_decremental = "grm",
+		},
+	},
+	playground = {
+		enable = true,
+		disable = {},
+		updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+		persist_queries = false, -- Whether the query persists across vim sessions
+		keybindings = {
+			toggle_query_editor = 'o',
+			toggle_hl_groups = 'i',
+			toggle_injected_languages = 't',
+			toggle_anonymous_nodes = 'a',
+			toggle_language_display = 'I',
+			focus_language = 'f',
+			unfocus_language = 'F',
+			update = 'R',
+			goto_node = '<cr>',
+			show_help = '?',
+		},
+	},
+	textobjects = {
+		select = {
+			enable = true,
+
+			-- Automatically jump forward to textobj, similar to targets.vim 
+			lookahead = true,
+
+			keymaps = {
+				-- You can use the capture groups defined in textobjects.scm
+				["af"] = "@function.outer",
+				["if"] = "@function.inner",
+				["ac"] = "@class.outer",
+				["ic"] = "@class.inner",
+
+				-- Or you can define your own textobjects like this
+				["iF"] = {
+					python = "(function_definition) @function",
+					cpp = "(function_definition) @function",
+					c = "(function_definition) @function",
+					java = "(method_declaration) @function",
+				},
+			},
+		},
+		swap = {
+			enable = true,
+			swap_next = {
+				["<leader>a"] = "@parameter.inner",
+			},
+			swap_previous = {
+				["<leader>A"] = "@parameter.inner",
+			},
+		},
+		move = {
+			enable = true,
+			set_jumps = true, -- whether to set jumps in the jumplist
+			goto_next_start = {
+				["]m"] = "@function.outer",
+				["]]"] = "@class.outer",
+			},
+			goto_next_end = {
+				["]M"] = "@function.outer",
+				["]["] = "@class.outer",
+			},
+			goto_previous_start = {
+				["[m"] = "@function.outer",
+				["[["] = "@class.outer",
+			},
+			goto_previous_end = {
+				["[M"] = "@function.outer",
+				["[]"] = "@class.outer",
+			},
+		},
+	},
+	refactor = {
+		highlight_current_scope = { enable = true },
+		smart_rename = {
+			enable = true,
+			keymaps = {
+				smart_rename = "grr",
+			},
+		},
+		navigation = {
+			enable = true,
+			keymaps = {
+				goto_definition = "gnd",
+				list_definitions = "gnD",
+				list_definitions_toc = "gO",
+				goto_next_usage = "<a-*>",
+				goto_previous_usage = "<a-#>",
+			},
+		},
+	},
 }
 
 require('neoscroll').setup()
 
--- require('bufferline').setup {
---   options = {
---     numbers = "ordinal" ,
---     number_style = { "none", "none" }, -- buffer_id at index 1, ordinal at index 2
---     mappings = true ,
---     diagnostics = "nvim_lsp",
---     persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
---     -- can also be a table containing 2 custom separators
---     -- [focused and unfocused]. eg: { '|', '|' }
---     show_buffer_icons = false,
---     separator_style = { '', '' },
---   }
--- }
+require('bufferline').setup {
+  options = {
+    numbers = "ordinal" ,
+    number_style = { "none", "none" }, -- buffer_id at index 1, ordinal at index 2
+    mappings = true ,
+    diagnostics = "nvim_lsp",
+    persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
+    -- can also be a table containing 2 custom separators
+    -- [focused and unfocused]. eg: { '|', '|' }
+    show_buffer_icons = false,
+    separator_style = { '', '' },
+  }
+}
+
+require('rust-tools').setup {
+  tools = { 
+    -- rust-tools options
+    -- automatically set inlay hints (type hints)
+    -- There is an issue due to which the hints are not applied on the first
+    -- opened file. For now, write to the file to trigger a reapplication of
+    -- the hints or just run :RustSetInlayHints.
+    -- default: true
+    autoSetHints = true,
+
+    -- whether to show hover actions inside the hover window
+    -- this overrides the default hover handler
+    -- default: true
+    hover_with_actions = true,
+
+    runnables = {
+      -- whether to use telescope for selection menu or not
+      -- default: true
+      use_telescope = true
+
+      -- rest of the opts are forwarded to telescope
+    },
+
+    inlay_hints = {
+      -- wheter to show parameter hints with the inlay hints or not
+      -- default: true
+      show_parameter_hints = true,
+
+      -- prefix for parameter hints
+      -- default: "<-"
+      parameter_hints_prefix = "<-",
+
+      -- prefix for all the other hints (type, chaining)
+      -- default: "=>"
+      other_hints_prefix  = "=>",
+
+      -- whether to align to the lenght of the longest line in the file
+      max_len_align = false,
+
+      -- padding from the left if max_len_align is true
+      max_len_align_padding = 1,
+
+      -- whether to align to the extreme right or not
+      right_align = false,
+
+      -- padding from the right if right_align is true
+      right_align_padding = 7,
+    },
+
+    hover_actions = {
+      -- the border that is used for the hover window
+      -- see vim.api.nvim_open_win()
+      border = {
+        {"╭", "FloatBorder"},
+        {"─", "FloatBorder"},
+        {"╮", "FloatBorder"},
+        {"│", "FloatBorder"},
+        {"╯", "FloatBorder"},
+        {"─", "FloatBorder"},
+        {"╰", "FloatBorder"},
+        {"│", "FloatBorder"}
+      },
+    },
+  },
+
+  -- all the opts to send to nvim-lspconfig
+  -- these override the defaults set by rust-tools.nvim
+  -- see https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md#rust_analyzer
+  server = {}, -- rust-analyer options
+}
 
 vim.cmd [[
 nnoremap <F7> :CHADopen<CR>
 set clipboard+=unnamedplus
-let g:tokyonight_style = "night"
-let g:tokyonight_italic_comments = 0
-let g:tokyonight_italic_keywords = 0
-let g:tokyonight_italic_functions = 0
-let g:tokyonight_hide_inactive_statusline = 1
-let g:gruvbox_italic = 0
-let g:gruvbox_italicize_comments = 0
-let g:gruvbox_italicize_strings = 0
-
-function! CustomizeColors()
- exec 'highlight CursorLine    ctermbg=236 guibg=#444444 cterm=none gui=none'
- exec 'highlight CursorColumn  ctermbg=236 guibg=#444444'
-endfunction
-augroup OnColorScheme
-	autocmd!
-	autocmd ColorScheme,BufEnter,BufWinEnter * call CustomizeColors()
-augroup END
-
-colorscheme gruvbox-material
-" colorscheme tokyonight
-" highlight CursorLine    ctermbg=236 guibg=#444444 cterm=none gui=none
-" highlight CursorColumn  ctermbg=236 guibg=#444444
+let g:gruvbox_italic_keywords   = 0
+let g:gruvbox_italic_comments   = 0
+let g:gruvbox_italic_functions  = 0
+let g:gruvbox_italic_variables  = 0
+let g:gruvbox_flat_style = "hard"
+let g:chadtree_settings = { "theme.text_colour_set": "nerdtree_syntax_dark" }
+syntax off
+autocmd FileType tcl,perl,lisp,clojure,scheme,vim syntax on
+colorscheme gruvbox-flat
+set guicursor=
 ]]
