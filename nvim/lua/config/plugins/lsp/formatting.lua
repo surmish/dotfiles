474787af1c659c37c6a4639dd85fba3cdabb9837
Urmish Shah <33273194+surmish@users.noreply.github.com>
2022-12-21 14:01:22 -0800

    Update formatting.lua

--- nvim/lua/config/plugins/lsp/formatting.lua
+++ nvim/lua/config/plugins/lsp/formatting.lua
@@ -29,20 +29,8 @@ end
 
 function M.setup(client, buf)
   local ft = vim.api.nvim_buf_get_option(buf, "filetype")
-  local nls = require("config.plugins.null-ls")
 
   local enable = false
-  if nls.has_formatter(ft) then
-    enable = client.name == "null-ls"
-  else
-    enable = not (client.name == "null-ls")
-  end
-
-  if client.name == "tsserver" then
-    enable = false
-  end
-
-  -- util.info(client.name .. " " .. (enable and "yes" or "no"), "format")
 
   client.server_capabilities.documentFormattingProvider = enable
   -- format on save
