fc52abe1756a8275c266a8fc117e4c7cc66c09db
Urmish Shah <33273194+surmish@users.noreply.github.com>
2023-01-04 18:20:28 -0800

    Update keymaps.lua

--- nvim/lua/lazyvim/config/keymaps.lua
+++ nvim/lua/lazyvim/config/keymaps.lua
@@ -1,5 +1,7 @@
 -- This file is automatically loaded by plugins.config
 
+local util = require("lazyvim.util")
+
 -- navigate command keymaps
 vim.keymap.set({ "n", "v", "x", "o" }, ";", "<Right>", { noremap = true, desc = "Move to the right" })
 vim.keymap.set({ "n", "v", "x", "o" }, "l", "<Up>", { noremap = true, desc = "Move up" })
@@ -52,7 +54,7 @@ end, { noremap = true, desc = "[sc] Toggle sign column" })
 vim.keymap.set("n", "k", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 vim.keymap.set("n", "l", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 
-vim.keymap.set("n", "<leader>q", ":q<CR>", { noremap = true, desc = "Exit if file not modified" })
+-- vim.keymap.set("n", "<leader>q", ":q<CR>", { noremap = true, desc = "Exit if file not modified" })
 vim.keymap.set("n", "<leader>Q", ":q!<CR>", { noremap = true, desc = "Force exit irrespective of changes" })
 vim.keymap.set("n", "<leader>x", ":x<CR>", { noremap = true, desc = "Close current buffer." })
 vim.keymap.set("n", "<leader><C-h>", "<ESC>:set wrap!<CR>", { noremap = true, desc = "Toggle wrap around long lines" })
@@ -151,8 +153,51 @@ vim.keymap.set("n", "<C-c>", "<cmd>normal ciw<cr>a")
 vim.keymap.set("v", "<", "<gv")
 vim.keymap.set("v", ">", ">gv")
 
+-- lazy
+vim.keymap.set("n", "<leader>l", "<cmd>:Lazy<cr>")
+
 -- new file
 vim.keymap.set("n", "<leader>fn", "<cmd>enew<cr>", { desc = "New File" })
 
+vim.keymap.set("n", "<leader>xl", "<cmd>lopen<cr>", { desc = "Open Location List" })
+vim.keymap.set("n", "<leader>xq", "<cmd>copen<cr>", { desc = "Open Quickfix List" })
+
+-- stylua: ignore start
+
+-- toggle options
+vim.keymap.set("n", "<leader>tf", require("lazyvim.plugins.lsp.format").toggle, { desc = "Format on Save" })
+vim.keymap.set("n", "<leader>ts", function() util.toggle("spell") end, { desc = "Spelling" })
+vim.keymap.set("n", "<leader>tw", function() util.toggle("wrap") end, { desc = "Word Wrap" })
+vim.keymap.set("n", "<leader>tn", function() util.toggle("relativenumber", true) util.toggle("number") end, { desc = "Line Numbers" })
+
+-- lazygit
+vim.keymap.set("n", "<leader>gg", function() require("lazyvim.util").float_term({ "lazygit" }) end, { desc = "Lazygit (cwd)" })
+vim.keymap.set("n", "<leader>gG", function() util.float_term({ "lazygit" }, { cwd = util.get_root() }) end, { desc = "Lazygit (root dir)" })
+
+-- quit
+vim.keymap.set("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit all" })
+
 -- highlights under cursor
-vim.keymap.set("n", "<leader>hl", vim.show_pos, { desc = "Highlight Groups at cursor" })
+if vim.fn.has("nvim-0.9.0") == 1 then
+  vim.keymap.set("n", "<leader>hl", vim.show_pos, { desc = "Highlight Groups at cursor" })
+end
+
+-- windows
+vim.keymap.set("n", "<leader>ww", "<C-W>p", { desc = "other-window" })
+vim.keymap.set("n", "<leader>wd", "<C-W>c", { desc = "delete-window" })
+vim.keymap.set("n", "<leader>w-", "<C-W>s", { desc = "split-window-below" })
+vim.keymap.set("n", "<leader>w|", "<C-W>v", { desc = "split-window-right" })
+
+-- tabs
+vim.keymap.set("n", "<leader><tab>l", "<cmd>tablast<CR>", { desc = "Last" })
+vim.keymap.set("n", "<leader><tab>f", "<cmd>tabfirst<CR>", { desc = "First" })
+vim.keymap.set("n", "<leader><tab><tab>", "<cmd>tabnew<CR>", { desc = "New Tab" })
+vim.keymap.set("n", "<leader><tab>]", "<cmd>tabnext<CR>", { desc = "Next" })
+vim.keymap.set("n", "<leader><tab>d", "<cmd>tabclose<CR>", { desc = "Close" })
+vim.keymap.set("n", "<leader><tab>[", "<cmd>tabprevious<CR>", { desc = "Previous" })
+
+-- buffers
+vim.keymap.set("n", "<leader>b]", "<cmd>:BufferLineCycleNext<CR>", { desc = "Next Buffer" })
+vim.keymap.set("n", "<leader>bb", "<cmd>:e #<cr>", { desc = "Switch to Other Buffer" })
+vim.keymap.set("n", "<leader>b[", "<cmd>:BufferLineCyclePrev<CR>", { desc = "Previous Buffer" })
+vim.keymap.set("n", "<leader>`", "<cmd>:e #<cr>", { desc = "Switch to Other Buffer" })
