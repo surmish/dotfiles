7cbe6d8f9091edcf951e763d69d5122d83198713
Urmish Shah <33273194+surmish@users.noreply.github.com>
2023-01-06 17:26:44 -0800

    Update colorscheme.lua

--- nvim/lua/lazyvim/plugins/colorscheme.lua
+++ nvim/lua/lazyvim/plugins/colorscheme.lua
@@ -17,19 +17,5 @@ return {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
-    lazy = false,
-    config = function()
-      require("catppuccin").setup({
-        flavour = "mocha",
-        dim_inactive = {
-          enabled = true,
-          percentage = 0.10,
-        },
-        integrations = {
-          gitsigns = false,
-        }
-      })
-      vim.cmd("colorscheme catppuccin")
-    end,
   },
 }
