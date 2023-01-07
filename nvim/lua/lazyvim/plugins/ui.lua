e69b0bff95eb8c7b0c91e05b9b44100443ba6af7
Urmish Shah <33273194+surmish@users.noreply.github.com>
2023-01-06 18:58:33 -0800

    Update ui.lua

--- nvim/lua/lazyvim/plugins/ui.lua
+++ nvim/lua/lazyvim/plugins/ui.lua
@@ -1,4 +1,29 @@
 return {
+
+  -- floating winbar
+  {
+    "b0o/incline.nvim",
+    version = false,
+    event = "BufReadPre",
+    config = function()
+      local colors = require("tokyonight.colors").setup()
+      require("incline").setup({
+        highlight = {
+          groups = {
+            InclineNormal = { guibg = "#FC56B1", guifg = colors.black },
+            InclineNormalNC = { guifg = "#FC56B1", guibg = colors.black },
+          },
+        },
+        window = { margin = { vertical = 0, horizontal = 1 } },
+        render = function(props)
+          local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+          local icon, color = require("nvim-web-devicons").get_icon_color(filename)
+          return { { icon, guifg = color }, { " " }, { filename } }
+        end,
+      })
+    end,
+  },
+
   -- better vim.notify
   {
     "rcarriga/nvim-notify",
@@ -63,6 +88,42 @@ return {
         },
       },
     },
+  },
+
+  -- animations
+  {
+    "echasnovski/mini.animate",
+    event = "VeryLazy",
+    config = function()
+      local mouse_scrolled = false
+      for _, scroll in ipairs({ "Up", "Down" }) do
+        local key = "<ScrollWheel" .. scroll .. ">"
+        vim.keymap.set("", key, function()
+          mouse_scrolled = true
+          return key
+        end, { expr = true })
+      end
+
+      local animate = require("mini.animate")
+
+      animate.setup({
+        resize = {
+          timing = animate.gen_timing.linear({ duration = 100, unit = "total" }),
+        },
+        scroll = {
+          timing = animate.gen_timing.linear({ duration = 150, unit = "total" }),
+          subscroll = animate.gen_subscroll.equal({
+            predicate = function(total_scroll)
+              if mouse_scrolled then
+                mouse_scrolled = false
+                return false
+              end
+              return total_scroll > 1
+            end,
+          }),
+        },
+      })
+    end,
   },
 
   -- statusline
