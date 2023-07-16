32e29b59f2519bb3b4140d3e8625dc57f72d765a
urmish <s.urmish@gmail.com>
2023-07-15 20:48:07 -0700

    fix cmp suggestions, snippets should be at the top

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -49,4 +49,36 @@ return {
       end,
     },
   },
+  -- Modify nvim-cmp
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      opts.sorting.comparators = {
+        cmp.config.compare.offset,
+        cmp.config.compare.exact,
+        cmp.config.compare.score,
+        function(a1, a2)
+          if a1 == "Snippet" and a2 == "Variable" or a2 == "Function" then
+            return true
+          end
+          return false
+        end,
+        cmp.config.compare.sort_text,
+        cmp.config.compare.length,
+        cmp.config.compare.order,
+      }
+      -- local types = require("cmp.types")
+      -- -- Function to sort LSP snippets, so that they appear at the end of LSP suggestions
+      -- local function prioritize_snippet(entry1, entry2)
+      --   if entry1:get_kind() == types.lsp.CompletionItemKind.Snippet then
+      --     return true
+      --   end
+      --   if entry2:get_kind() == types.lsp.CompletionItemKind.Snippet then
+      --     return false
+      --   end
+      -- end
+      -- table.insert(opts.sorting.comparators, 1, prioritize_snippet)
+    end,
+  },
 }
