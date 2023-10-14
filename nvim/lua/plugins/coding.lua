c860e1d1c114cde487938299ecb3bb51832fe5f7
Urmish Shah <33273194+surmish@users.noreply.github.com>
2023-10-14 11:19:30 -0700

    Update coding.lua

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -103,22 +103,27 @@ return {
   {
     "kawre/leetcode.nvim",
     build = ":TSUpdate html",
-    priority = 1000,
     dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "nvim-telescope/telescope.nvim",
-      "nvim-lua/plenary.nvim", -- required by telescope
       "MunifTanjim/nui.nvim",
-
-      -- optional
+      "nvim-lua/plenary.nvim", -- required by telescope
+      "nvim-telescope/telescope.nvim",
       "nvim-tree/nvim-web-devicons",
-
-      -- recommended
-      -- "rcarriga/nvim-notify",
+      "nvim-treesitter/nvim-treesitter",
+      "rcarriga/nvim-notify",
     },
     opts = {
-      -- configuration goes here
+      lang = "rust",
+      -- sql = "postgresql"
     },
+    config = function(_, opts)
+      vim.keymap.set("n", "<leader>lq", "<cmd>LcQuestionTabs<cr>")
+      vim.keymap.set("n", "<leader>lm", "<cmd>LcMenu<cr>")
+      vim.keymap.set("n", "<leader>lc", "<cmd>LcConsole<cr>")
+      vim.keymap.set("n", "<leader>ll", "<cmd>LcLanguage<cr>")
+      vim.keymap.set("n", "<leader>ld", "<cmd>LcDescriptionToggle<cr>")
+
+      require("leetcode").setup(opts)
+    end,
   },
 
   -- {
