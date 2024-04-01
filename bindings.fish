1281ce2853fe677045e0b9dead4c04f320b4526e
Urmish Shah <33273194+surmish@users.noreply.github.com>
2024-04-01 14:31:34 -0700

    Create bindings.fish

---
+++ bindings.fish
@@ -0,0 +1,14 @@
+set -g fish_key_bindings fish_vi_key_bindings
+
+bind -M default j  'backward-char'
+bind -M default \; 'forward-char'
+bind -M default k  ' '
+bind -M default l  ' '
+
+for mode in insert default visual
+  bind -M $mode \ca 'beginning-of-line'
+  bind -M $mode \ce 'end-of-line'
+  bind -M $mode \cp 'up-or-search'
+  bind -M $mode \cn 'down-or-search'
+  bind -M $mode \cj 'execute'
+end
